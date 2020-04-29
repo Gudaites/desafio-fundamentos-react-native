9ec40d1eb6a3d1d3c1133d27fbfc311889cd8f7e
Henrique Gudaites <52537422+Gudaites@users.noreply.github.com>
2020-04-28 21:16:07 -0300

    Challenge Completed

---
+++ src/hooks/cart.tsx
@@ -0,0 +1,131 @@
+import React, {
+  createContext,
+  useState,
+  useCallback,
+  useContext,
+  useEffect,
+} from 'react';
+
+import AsyncStorage from '@react-native-community/async-storage';
+import { Alert } from 'react-native';
+
+interface Product {
+  id: string;
+  title: string;
+  image_url: string;
+  price: number;
+  quantity: number;
+}
+
+interface CartContext {
+  products: Product[];
+  addToCart(item: Product): void;
+  increment(id: string): void;
+  decrement(id: string): void;
+}
+
+const CartContext = createContext<CartContext | null>(null);
+
+const CartProvider: React.FC = ({ children }) => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    async function loadProducts(): Promise<void> {
+      const storedProducts = await AsyncStorage.getItem('@cart');
+      console.log(storedProducts)
+      if (storedProducts) {
+        setProducts(JSON.parse(storedProducts));
+      }
+    }
+    loadProducts();
+  }, []);
+
+  const addToCart = useCallback(async product => {
+    let newList = [];
+      if (products.find(p => p.id === product.id)) {
+        newList = products.map(item => {
+          if (item.id === product.id) {
+            item.quantity += 1;
+          }
+          return item;
+        });
+      } else {
+        newList = [
+          ...products,
+          {
+            ...product,
+            quantity: 1,
+          },
+        ];
+      }
+
+      setProducts(newList);
+
+      await AsyncStorage.setItem('@cart', JSON.stringify(newList));
+  }, [products]);
+
+  const increment = useCallback(
+    async id => {
+      const incrementProds = products.map(p => {
+        if (p.id === id) {
+          p.quantity += 1;
+        }
+        return p;
+      });
+      setProducts(incrementProds);
+
+      await AsyncStorage.setItem('@cart', JSON.stringify(incrementProds));
+    },
+    [products],
+  );
+
+  const decrement = useCallback(
+    async id => {
+      const decrementProds = products.map(p => {
+        if (p.id === id) {
+          // if (!(p.quantity - 1)) {
+          //   return p;
+          // }
+          p.quantity -= 1;
+        }
+        return p;
+      });
+
+      const teste = decrementProds.findIndex(p => {
+        if(p.id === id && p.quantity === 0){
+          return p;
+        }
+        return;
+      })
+
+      if(teste >= 0){
+        decrementProds.splice(teste, 1);
+        Alert.alert('Sucesso', 'Produto removido com sucesso')
+      }
+
+      setProducts(decrementProds);
+      await AsyncStorage.setItem('@cart', JSON.stringify(decrementProds));
+
+    },
+    [products],
+  );
+
+  const value = React.useMemo(
+    () => ({ addToCart, increment, decrement, products }),
+    [products, addToCart, increment, decrement],
+  );
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+function useCart(): CartContext {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw new Error(`useCart must be used within a CartProvider`);
+  }
+
+  return context;
+}
+
+export { CartProvider, useCart };
